b8593a052015e19a4a9a796715384e31a3068ee0
Shirshajit Sen Gupta <shirshajit@gmail.com>
2023-03-13 23:32:53 +0800

    Created dataloaders closes #18

---
+++ data/README.md
@@ -0,0 +1,21 @@
+# Data Description
+
+There are three columns in the ELCo dataset. 
+
+- English: The English phrase.
+- Emoji: The corresponding emoji sequence.
+- emoji_list: Another format of emoji of the emoji sequence, easier to be processed by modern NLP systems. 
+
+
+
+# Dataset Statistics
+
+Please refer to `ELCo-defense.pdf`
+
+
+
+# Policy
+
+Please help us keep all the material (dataset, slides, etc) confidential because we have a paper under submission and it's important to keep it anonymous and not advertised on social media (Twitter/ins..). 
+
+If you wish to PR your CS4248 final project, please go for another dataset!
---
+++ elco_dataloader.py
@@ -0,0 +1,123 @@
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from itertools import chain
+
+"""
+    Class for loading the Elco dataset.
+    The dataset is a csv file with the following format:
+    label, emojis separated by [EM], list of emojis
+"""
+class ElcoDataset(Dataset): 
+    """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+
+        Notes:
+            Each label has 7,8, or 9 rows, each row has a different list of emojis
+            Emoji and emoji_list columns hold the same information, but in different formats
+    """
+    def __init__(self, data, labels, transform=None): 
+        self.transform = transform
+        self.labels = labels
+        self.data = data
+        
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __getitem__(self, idx) -> tuple:
+        label = self.labels[idx]
+        data = self.data[label]
+        
+        if self.transform:
+            data = self.transform(data)
+        return data, label
+    
+    def get_data(self) -> dict:
+        return self.data
+    
+    def get_labels(self) -> list:
+        return self.labels
+
+class ElcoDataLoader:
+    def __init__(self, csv_file: str, batch_size=1, shuffle=True, num_workers=0, 
+                 transform=None, test_size=0.2):
+        self.train, self.test = ElcoDataset(csv_file, transform=transform, 
+                                            test_size=test_size).get_data()
+        self.train_loader = DataLoader(self.train, batch_size=batch_size, 
+                                       shuffle=shuffle, num_workers=num_workers)
+        self.test_loader = DataLoader(self.test, batch_size=batch_size, 
+                                      shuffle=shuffle, num_workers=num_workers)
+    
+    def get_dataset(self) -> ElcoDataset:
+        return self.dataset
+    
+    def get_labels(self) -> list:
+        return self.dataset.get_labels()
+    
+    def get_data(self) -> dict:
+        return self.dataset.get_data()
+    
+    def get_data_by_label(self, label) -> dict:
+        return self.dataset.get_data()[label]
+
+def get_loaders(csv_file: str, batch_size=1, shuffle=True, num_workers=0, 
+                transform=None, test_size=0.2):
+    """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+            
+        Returns:
+            train_loader, test_loader
+        
+        Notes:
+            Each emoji in the dataloader is a tuple 
+        e.g. 
+        [{
+            0: [('shorts',), ('electric plug',)],
+            1: [('pinching hand',), ('light bulb',), ('high voltage',)],
+            2: [('cross mark',), ('pencil',), ('axe',), ('wrench',)],
+            3: [('cross mark button',), ('potable water',)],
+            4: [('fast down button',),
+            ('coat',),
+            ('woman’s clothes',),
+            ('t-shirt',),
+            ('jeans',)],
+            5: [('double exclamation mark',), ('warning',), ('bread',)],
+            6: [('hut',), ('cross mark',)],
+            7: [('pinching hand',), ('package',)]
+            },
+        ('short supply',)]
+    """
+    df = pd.read_csv(csv_file)
+    labels = df["English"].unique()
+    train_data, test_data = {}, {}
+    train_labels, test_labels = train_test_split(labels, test_size=test_size)
+    for label in train_labels:
+        rank_dict = {}
+        sentences = df.loc[df["English"] == label, "Emoji"].apply(
+            lambda x: list(filter(None, x.split("[EM]")))).tolist()
+        
+        for i in range(len(sentences)):
+            rank_dict[i] = sentences[i]
+
+        train_data[label] = rank_dict
+    for label in test_labels:
+        rank_dict = {}
+        sentences = df.loc[df["English"] == label, "Emoji"].apply(
+            lambda x: list(filter(None, x.split("[EM]")))).tolist()
+        for i in range(len(sentences)):
+            rank_dict[i] = sentences[i]
+
+        test_data[label] = rank_dict
+    train_data = ElcoDataset(train_data, train_labels, transform=transform)
+    test_data = ElcoDataset(test_data, test_labels, transform=transform)
+    train_loader = DataLoader(train_data, batch_size=batch_size,
+                                shuffle=shuffle, num_workers=num_workers)
+    test_loader = DataLoader(test_data, batch_size=batch_size,
+                                shuffle=shuffle, num_workers=num_workers)
+    return train_loader, test_loader
